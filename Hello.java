9b5b0abacd7fa21cd3afa94c93173b96328f53f5
spacefoul <lightturtle8@gmail.com>
2024-10-08 02:04:04 +0900

    10/07

---
+++ Hello.java
@@ -0,0 +1,54 @@
+public class Hello {
+    private String toWhom = "world";
+    private String whom;
+
+    Hello() {}
+
+    Hello(String whom) {
+        setWhom(whom);
+    }
+
+    public String getWhom() {
+        return whom;
+    }
+
+    public void setWhom(String whom) {
+        this.toWhom = whom;
+    }
+
+    public void sayHello() {
+        System.out.println("hello " + getWhom());
+    }
+
+    void print() {
+        this.sayHello();
+    }
+
+
+
+
+}
+class hello2 {
+    public static void main (String[] args) {
+        Hello[] arr = new Hello[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = new Hello();
+        }
+
+        for (Hello a : arr) {
+            a.sayHello();
+        }
+
+
+        Hello[] hellos = new Hello[3];
+        String[] names = {"Kim", "Kang", "Lee"};
+        for (int i = 0; i < 3; i++) {
+
+        }
+
+
+
+
+    }
+
+}
--- main.java
+++ main.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 class main {
     public static void main(String[] args) {
-        double arr1[] = new double[4];
-        float arr[] = {1.1f, 2.3f, 4.7f, 7.5f};
 
-        System.out.printf("배열 크기: %d\n", arr.length);
-        for (int i = 0; i < arr.length; i++) {
-            arr1[i] = arr[i];
-        }
-        System.out.printf("arr[0] = %f ", arr1[0]);
-        System.out.printf("arr[1] = %f ", arr1[1]);
-        System.out.println("arr[3] = " + arr1[3]);
-        System.out.printf("arr[2] = %f ", arr1[2]);
-        System.out.printf("arr[3] = %f ", arr1[3]);
-
+        boolean b = 3 > 2;
+        String str = "3 > 2는 " +b+ "입니다.";
+        System.out.printf(str);
 
     }
 }
